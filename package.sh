f3fdb6121d3375a7b8d506846957b9d0653ed403
Oliver Lowe <o@olowe.co>
2020-12-22 23:37:15 +1100

    script packaging workflow
    This is dumb but effective for just one person like me

---
+++ package.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# creates a tarball in /tmp to be uploaded to github releases
+
+GOOS=linux
+GOARCH=amd64
+version=`git tag |tail -1`
+
+go build 
+
+mkdir -p /tmp/check_wildflyfree
+cp check_wildflyfree /tmp/check_wildflyfree
+cp check_wildflyfree.1 /tmp/check_wildflyfree
+cp README.md /tmp/check_wildflyfree
+cp icinga2.conf /tmp/check_wildflyfree
+cp LICENSE /tmp/check_wildflyfree
+
+(cd /tmp && tar cv check_wildflyfree | gzip -c > check_wildflyfree-$tag-${GOOS}_${GOARCH}.tar.gz)
